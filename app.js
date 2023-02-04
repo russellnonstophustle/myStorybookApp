8222fc7f46e055e591298da5608179bf82fb32e4
Chris Russell <crussell8690@gmail.com>
2023-02-03 16:15:37 -0800

    cyclic fix connectDb then listen

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ dotenv.config({path: './config/config.env'})
 // Passport config
 require('./config/passport')(passport)
 
-connectDB()
-
 const app = express()
+const PORT = process.env.PORT || 3000
+
 
 // Body Parser
 app.use(express.urlencoded({ extended: false}))
@@ -85,9 +85,11 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
 app.use('/stories', require('./routes/stories'))
-const PORT = process.env.PORT || 3000
 
 
-app.listen(
-    PORT, 
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+connectDB().then(() => {
+  app.listen(
+    PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+    })
+  })
